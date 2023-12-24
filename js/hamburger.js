b3783e8aebe45dc5a707ad457fb34691e78d802c
LibertyReman <libertyreman@gmail.com>
2023-12-25 08:47:29 +0900

    ハンバーガーメニューの追加

---
+++ js/hamburger.js
@@ -0,0 +1,9 @@
+document.querySelector( '.js-hamburger' ).addEventListener(
+    'click',
+    () => {
+        document.querySelector('.js-hamburger').classList.toggle('is-open');
+        document.querySelector('.p-gmenu').classList.toggle('is-open');
+        document.querySelector('.l-main').classList.toggle('is-open');
+        document.querySelector('body').classList.toggle('is-open');
+    }
+);
