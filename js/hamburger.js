c1dc1bd7d99ec541362505cf8ab67002c8fe2b1b
LibertyReman <libertyreman@gmail.com>
2024-01-04 05:15:03 +0900

    メニューにページ内リンクを追加

--- js/hamburger.js
+++ js/hamburger.js
@@ -4,6 +4,17 @@ document.querySelector( '.js-hamburger' ).addEventListener(
         document.querySelector('.js-hamburger').classList.toggle('is-open');
         document.querySelector('.p-gmenu').classList.toggle('is-open');
         document.querySelector('.l-main').classList.toggle('is-open');
-        document.querySelector('body').classList.toggle('is-open');
     }
 );
+
+// メニューがクリックされたらハンバーガーを閉じる
+document.querySelector( '.js-hamburger-list' ).addEventListener(
+    'click',
+    () => {
+        document.querySelector('.js-hamburger').classList.remove('is-open');
+        document.querySelector('.p-gmenu').classList.remove('is-open');
+        document.querySelector('.l-main').classList.remove('is-open');
+    }
+);
+
+
