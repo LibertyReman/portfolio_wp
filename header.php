a1c1b455aaa2365a16d4238c25bcd168fdccecac
LibertyReman <libertyreman@gmail.com>
2024-01-08 08:13:43 +0900

    header.phpにテンプレートタグを追加

--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="<?php language_attributes(); ?>"> <!-- 言語設定を自動的に出力 -->
   <head>
     <meta chaarset="utf-8">
     <meta name="viewport" content="width=width=device-width, initial-scale=1.0">
-    <meta name="description" content="Liber's Portfolio">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
     <title>Liber's Portfolio</title>
     <link href="./img/hero.jpg" as="image" rel="preload">
     <link href="./css/ress.css" rel="stylesheet">
@@ -14,7 +14,7 @@
     <?php wp_head(); ?> <!-- 必ず入れる プラグインや情報出力が機能しなくなってしまう -->
   </head>
 
-  <body>
+  <body <?php body_class(); ?>> <!-- 表示するページの種類によってclassを付与 -->
     <header class="l-header">
       <div class="c-inner">
         <?php get_sidebar(); ?> <!-- sidebar.phpを読み込むテンプレートタグ -->
