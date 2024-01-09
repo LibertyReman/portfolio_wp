3d86db0bea7833792121d15604e8b3d52a498748
LibertyReman <libertyreman@gmail.com>
2024-01-10 04:32:38 +0900

    Contact Form 7 プラグインの追加

---
+++ page-contact.php
@@ -0,0 +1,7 @@
+        <section class="p-contact c-section"> <!-- flex -->
+          <h2 class="c-section__title u-pb-5px" id="contact">Contact</h2>
+          <div class="p-contact__form">
+            <?php echo do_shortcode('[contact-form-7 id="34ec9df" title="Contact"]'); ?>
+          </div>
+        </section>
+
